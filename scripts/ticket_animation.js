188f2b57e76a26b2b22c7519a5e3e4d2b094d206
TchockDonwood <savilovsem@gmail.com>
2025-06-07 23:36:21 +0500

    сделал страницу topSeries

---
+++ scripts/ticket_animation.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll('.ticket-container').forEach((ticket, i) => {
+  const left = ticket.querySelector('.ticket-left');
+  const right = ticket.querySelector('.ticket-right');
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ticket,
+      start: `top ${60 + i * 10}%`, // ближе к верху и с небольшим сдвигом для каждого
+      end: "bottom 40%",
+      scrub: true,
+    }
+  })
+  .to(left, {
+    x: -150,
+    rotation: -10,
+    opacity: 0,
+    ease: "power2.out"
+  }, 0)
+  .to(right, {
+    x: 150,
+    rotation: 6,
+    opacity: 0,
+    ease: "power2.out"
+  }, 0);
+});
